759ab2d9cd34e0ce83231c4eefe0833a7a85107a
Eren Yavuz <109018781+erenyavuzs@users.noreply.github.com>
2024-08-13 15:56:18 +0300

    Work in progress

--- main.py
+++ main.py
@@ -40,9 +40,44 @@ def find_path(maze, stdscr):
     end = "X"
     start_pos = find_start(maze, start)
 
+    q = queue.Queue()
+    q.put((start_pos, [start_pos]))
 
+    visited = set()
 
+    while not q.empty():
+        current_pos, path = q.get()
+        row, col = current_pos
 
+        if maze[row][col] == end:
+            return path
+        
+        neighbors = find_neighbors(maze, row, col)
+        for neighbor in neighbors:
+            if neighbor in visited:
+                continue
+
+            r, c = neighbor
+            if maze[r][c] == "#":
+                continue
+
+            new_path = path + [neighbor]
+            q.put((neighbor, new_path))
+            visited.add(neighbor)
+
+def find_neighbors(maze, row, col):
+    neighbors = []
+
+    if row > 0: # up
+        neighbors.append((row -1, col))
+    if row + 1 < len(maze): # down
+        neighbors.append((row +1, col))
+    if col > 0: # left
+        neighbors.append((row, col -1))
+    if col + 1 < len(maze[0]):
+        neighbors.append((row, col + 1))
+
+    return neighbors
 
 
 
